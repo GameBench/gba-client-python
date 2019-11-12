e5d3046fc955fd099cb58e4d928f633d2b4a7bc8
Ade Slade <ade@gamebench.net>
2019-11-12 16:54:37 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gb-automation-client",
-    version="0.0.1",
+    version="0.0.2",
     author="Ade Slade",
     author_email="ade@gamebench.net",
     description="Python GameBench Automation (GBA) client",
