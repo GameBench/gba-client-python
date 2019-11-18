31a8a47b131d2fbaa4933e626a0b56ee5c81f4cc
Ade Slade <ade@gamebench.net>
2019-11-18 16:50:45 +0000

    API credentials are now configured in GBA server

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gb-automation-client",
-    version="0.0.2",
+    version="0.0.3",
     author="Ade Slade",
     author_email="ade@gamebench.net",
     description="Python GameBench Automation (GBA) client",
